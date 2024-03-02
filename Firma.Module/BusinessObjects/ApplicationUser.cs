9519db9a13fb4713e1852f1cd39b88a9613850a4
Grzegorz Kosiński <grzelu37@gmail.com>
2024-03-02 11:23:53 +0100

    Pierwsza wersja aplikacji klasa customer

---
+++ Firma.Module/BusinessObjects/ApplicationUser.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Text;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.Xpo;
+
+namespace Firma.Module.BusinessObjects;
+
+[MapInheritance(MapInheritanceType.ParentTable)]
+[DefaultProperty(nameof(UserName))]
+public class ApplicationUser : PermissionPolicyUser, ISecurityUserWithLoginInfo, ISecurityUserLockout {
+    private int accessFailedCount;
+    private DateTime lockoutEnd;
+
+    public ApplicationUser(Session session) : base(session) { }
+
+    [Browsable(false)]
+    public int AccessFailedCount {
+        get { return accessFailedCount; }
+        set { SetPropertyValue(nameof(AccessFailedCount), ref accessFailedCount, value); }
+    }
+
+    [Browsable(false)]
+    public DateTime LockoutEnd {
+        get { return lockoutEnd; }
+        set { SetPropertyValue(nameof(LockoutEnd), ref lockoutEnd, value); }
+    }
+
+    [Browsable(false)]
+    [Aggregated, Association("User-LoginInfo")]
+    public XPCollection<ApplicationUserLoginInfo> LoginInfo {
+        get { return GetCollection<ApplicationUserLoginInfo>(nameof(LoginInfo)); }
+    }
+
+    IEnumerable<ISecurityUserLoginInfo> IOAuthSecurityUser.UserLogins => LoginInfo.OfType<ISecurityUserLoginInfo>();
+
+    ISecurityUserLoginInfo ISecurityUserWithLoginInfo.CreateUserLoginInfo(string loginProviderName, string providerUserKey) {
+        ApplicationUserLoginInfo result = new ApplicationUserLoginInfo(Session);
+        result.LoginProviderName = loginProviderName;
+        result.ProviderUserKey = providerUserKey;
+        result.User = this;
+        return result;
+    }
+}
---
+++ Firma.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma.Module.BusinessObjects
+{
+
+
+    [DefaultClassOptions]
+    public class Customer : BaseObject
+    {
+        public Customer(Session session) : base(session)
+        { }
+
+
+        string notes;
+        string lastName;
+        string firstName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string FirstName
+        {
+            get => firstName;
+            set => SetPropertyValue(nameof(FirstName), ref firstName, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string LastName
+        {
+            get => lastName;
+            set => SetPropertyValue(nameof(LastName), ref lastName, value);
+        }
+        
+        [Size(SizeAttribute.Unlimited)]
+        public string Notes
+        {
+            get => notes;
+            set => SetPropertyValue(nameof(Notes), ref notes, value);
+        }
+    }
+}
