ea9cf0ccd304e47924cc0ca032e20ba084f24ee4
Grzegorz Kosiński <grzelu37@gmail.com>
2024-03-28 20:19:12 +0100

    dodanie copy paste poprawiaće poprzednie copy paste

--- GasStationApp/GasStationDto.cs
+++ GasStationApp/GasStationDto.cs
@@ -6,24 +6,26 @@ using System.Threading.Tasks;
 
 namespace GasStationApp
 {
+    public class GasStationResponseDto
+    {
+        public List<GasStationDto> GasStations { get; set; }
+        public int TotalCount { get; set; }
+    }
+
     public class GasStationDto
     {
         public int StationNumber { get; set; }
         public string StationName { get; set; }
         public string Country { get; set; }
-        public string Comments { get; set; }
         public string Currency { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
         public string CompanyName { get; set; }
-        public decimal ServiceSurcharge { get; set; }
+        public double ServiceSurcharge { get; set; }
         public bool ZeroServiceFee { get; set; }
-        public string Longitude { get; set; }
-        public string Latitude { get; set; }
-        public bool OmrStation { get; set; }
-        public bool HotSpot { get; set; }
         public DateTime ValidFrom { get; set; }
         public DateTime ValidTo { get; set; }
         public string PriceZone { get; set; }
     }
+
 }
