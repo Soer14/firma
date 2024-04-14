261a3e4123cec82ccac616db01461d51317212a3
Grzegorz Kosiński <grzelu37@gmail.com>
2024-04-14 12:59:59 +0200

    Station Controller

---
+++ Firma.Module/Controllers/StationAssetsLVController.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using Firma.Module.BusinessObjects.Kartoteki;
+using Firma.Module.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma.Module.Controllers
+{
+    public class StationAssetsLVController : ObjectViewController<ListView,GasStationAssets>
+    {
+        SimpleAction MySimpleAction;
+        public StationAssetsLVController() : base()
+        {
+            MySimpleAction = new SimpleAction(this, $"{GetType().FullName}-{nameof(MySimpleAction)}", DevExpress.Persistent.Base.PredefinedCategory.Unspecified)
+            {
+                Caption = "Importuj Dane",
+                ImageName = "BO_Skull"
+
+            };
+            MySimpleAction.Execute += MySimpleAction_Execute;
+            
+
+        }
+        private async void MySimpleAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+
+               
+            
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+
+}
