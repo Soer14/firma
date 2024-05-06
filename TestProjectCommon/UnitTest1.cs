69f1a65d1c0b6ba71d28711c895e142792f1918e
Grzegorz Kosiński <grzelu37@gmail.com>
2024-05-06 20:37:18 +0200

    .

--- TestProjectCommon/UnitTest1.cs
+++ TestProjectCommon/UnitTest1.cs
@@ -26,7 +26,7 @@ namespace TestProjectCommon
         [Test]
         [TestCase(1,5,6)]
         [TestCase(5, 5, 26)]
-        [TestCase(17, 6, 17)]
+        [TestCase(17, 9, 17)]
         [TestCase(11, 8, 20)]
         [TestCase(0, 4, 46)]
         [TestCase(null, 8, 6)]
